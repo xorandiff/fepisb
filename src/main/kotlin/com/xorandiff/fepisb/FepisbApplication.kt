6eede930f40b9a760724dc24ffc8a886dd04e487
xorandiff <roxav@protonmail.com>
2025-07-30 17:29:31 +0200

    Hello Github

---
+++ src/main/kotlin/com/xorandiff/fepisb/FepisbApplication.kt
@@ -0,0 +1,11 @@
+package com.xorandiff.fepisb
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FepisbApplication
+
+fun main(args: Array<String>) {
+	runApplication<FepisbApplication>(*args)
+}
---
+++ src/test/kotlin/com/xorandiff/fepisb/FepisbApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.xorandiff.fepisb
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FepisbApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
